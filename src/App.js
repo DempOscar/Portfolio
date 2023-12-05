2b54228a921859dc20198c4653314ec2ef8493dc
TheTeaKing12 <86569078+TheTeaKing12@users.noreply.github.com>
2023-12-05 16:19:13 +0000

    updated background and buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import AboutMe from './AboutMe';
+import Portfolio from './Portfolio';
+import ContactMe from './ContactMe';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-      </header>
-    </div>
+    <Router>
+    <nav>
+      <Link to="/" className="home-button">Home</Link> 
+      <Link to="/ContactMe.js" className="contactMe-button">ContactMe</Link>
+      <Link to="/Portfolio.js" className="portfolio-button">Portfolio</Link>
+      <Link to="/AboutMe.js" className="aboutme-button">AboutMe</Link> 
+      {/* Other navigation links */}
+    </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact-me" element={<ContactMe />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
