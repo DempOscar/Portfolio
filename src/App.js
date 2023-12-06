2f47079682bacdcccf4df6470c91cc1c724314bc
Demposcar <Oscardempseyw@gmail.com>
2023-12-06 16:05:46 +0000

    i deleted aboutme/portfiolo web pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import Home from './Home';
-import AboutMe from './AboutMe';
-import Portfolio from './Portfolio';
 import ContactMe from './ContactMe';
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
@@ -12,14 +10,11 @@ function App() {
     <Router>
     <nav>
       <Link to="/" className="home-button">Home</Link> 
-      <Link to="/ContactMe.js" className="contactMe-button">ContactMe</Link>
-      <Link to="/Portfolio.js" className="portfolio-button">Portfolio</Link>
+      <Link to="/ContactMe.js" className="contactme-button">ContactMe</Link>
     </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="src/about-me" element={<AboutMe />} />
-        <Route path="src/portfolio" element={<Portfolio />} />
-        <Route path="src/contact-me" element={<ContactMe />} />
+        <Route path="./ContactMe" element={<ContactMe />} />
       </Routes>
     </Router>
   );
