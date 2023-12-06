77f9674d4658fe1e1c24541fdb337694b10475a6
Demposcar <Oscardempseyw@gmail.com>
2023-12-06 17:19:03 +0000

    Added Blog page

---
+++ src/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        
+      <div className="home-background">
+        <h1>This is going to be my Blog</h1>
+      </div>
+    );
+  }
+
+export default Home;
