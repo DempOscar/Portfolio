64b1e55c4584a758ad3c7f6a5d17eb2c8db686c3
Andrej Zukov Gregoric <andrejzukovgregoric@gmail.com>
2023-12-06 16:59:31 +0000

    Added paragraph.

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -4,7 +4,9 @@ function ContactMe() {
     return (
         
       <div className="home-background">
-        <h1>ContactMe</h1>
+            <h1>Contact me anytime on ...</h1>
+
+              <p>Hello world! I'm a paragraph.</p>
       </div>
     );
   }
