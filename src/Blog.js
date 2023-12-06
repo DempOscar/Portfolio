7cbc9d6ee84773d248fd0f703e9a1304fce0a2f1
Demposcar <Oscardempseyw@gmail.com>
2023-12-06 17:52:18 +0000

    added blogged code and installed marked

--- src/Blog.js
+++ src/Blog.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {marked} from 'marked'; // A library to convert markdown to HTML
 
-function Home() {
-    return (
-        
-      <div className="home-background">
-        <h1>This is going to be my Blog</h1>
+function Blog() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch post list from the posts directory
+    const fetchPosts = async () => {
+      // Example: Fetching post file names from an API or static list
+      const postFiles = ['first_post.md', 'another_post.md']; // Add your post file names here
+      const postsData = await Promise.all(postFiles.map(async file => {
+        const response = await fetch(`/posts/${file}`);
+        console.log("Reading File")
+        const text = await response.text();
+        return {
+          slug: file.replace('.md', ''),
+          title: marked(text).match(/<h1>(.*?)<\/h1>/)[1], // Extract title from markdown
+          content: marked(text)
+        };
+      }));
+      setPosts(postsData);
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="home-background">
+      <h1>This is going to be my Blog</h1>
+      <div>
+        {posts.map(post => (
+          <div key={post.slug}>
+            <Link to={`/post/${post.slug}`}>{post.title}</Link>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-export default Home;
+export default Blog;
---
+++ src/Post.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import marked from 'marked';
+
+function Post() {
+  const { slug } = useParams();
+  const [postContent, setPostContent] = useState('');
+
+  useEffect(() => {
+    // Fetch the markdown content of the post
+    const fetchPostContent = async () => {
+      const response = await fetch(`/posts/${slug}.md`);
+      const text = await response.text();
+      setPostContent(marked(text));
+    };
+
+    fetchPostContent();
+  }, [slug]);
+
+  return (
+    <div className="post-content" dangerouslySetInnerHTML={{ __html: postContent }} />
+  );
+}
+
+export default Post;
