496addd41f5b80638613d75c3e3c101fbdf07107
Demposcar <Oscardempseyw@gmail.com>
2023-12-06 17:20:36 +0000

    Added Blog page to navigation bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Home';
 import ContactMe from './ContactMe';
+import Blog from './Blog';
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
@@ -11,10 +12,12 @@ function App() {
     <nav>
       <Link to="/" className="home-button">Home</Link> 
       <Link to="/ContactMe.js" className="contactme-button">ContactMe</Link>
+      <Link to="/Blog.js" className="Blog-button">Blog</Link>
     </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="ContactMe.js" element={<ContactMe />} />
+        <Route path="Blog.js" element={<Blog />} />
       </Routes>
     </Router>
   );
