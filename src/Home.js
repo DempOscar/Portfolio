44d8ac45b51456a4e258f795bd6d1ae54c8618d0
Demposcar <Oscardempseyw@gmail.com>
2023-12-07 19:57:17 +0000

    updated styles and added web page content

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 function Home() {
     return (
-        
-      <div className="home-background">
-        <h1>Welcome to My Website</h1>
-      </div>
+        <div className="home-background">
+            <div className="container">
+                <h1 className="bubble-title">Welcome To My Online Portfolio</h1>
+                <h3 classname="welcome-header">Hi! I am Oscar</h3>
+                <p classname="p1">I am a young frontend web developer with experience at at a "" backed company called Corrily where i expanded and improve on my programming skills using Html, css and React. </p>
+                <h2>About Me</h2>
+                <p>Ever since I discovered the art of programming, it has been an unceasing source of joy and fascination for me.  journey through a landscape of logic and creativity, where I get to play both the architect and the builder. The thrill of solving complex problems, the satisfaction of debugging code until it runs flawlessly, and the sheer pleasure of learning new languages and technologies make programming not just a skill, but a passion for me. It's in these moments of creation and problem-solving where I find my true calling, continuously inspired by the limitless potential of what can be built in the digital world.</p>
+
+            </div>
+        </div>
     );
-  }
+}
 
 export default Home;
