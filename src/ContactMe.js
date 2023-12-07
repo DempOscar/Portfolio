b581f78e0a02f936e35d8fe0711a06c55ea1d122
Demposcar <Oscardempseyw@gmail.com>
2023-12-07 19:56:52 +0000

    updated and styled

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -4,9 +4,19 @@ function ContactMe() {
     return (
         
       <div className="home-background">
-            <h1>Contact me anytime on ...</h1>
-
-              <p>Hello world! I'm a paragraph.</p>
+        <div>
+            <div className="contact-header">
+                <h1>Contact Me</h1>
+            </div>
+            <div classname="contact-p1">
+             <p>Any enquires here are my contact details:</p>
+            </div>
+            <div className="contact-info">
+                <p>Oscar Dempsey<br />
+                    <a href="mailto:OscarDempseyW@gmail.com">OscarDempseyW@gmail.com</a>
+                </p>
+            </div>
+        </div>
       </div>
     );
   }
