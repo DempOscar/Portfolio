dac6216ca092f5829369560779ee3e579c7eac57
Demposcar <Oscardempseyw@gmail.com>
2023-12-07 19:59:50 +0000

    added footer.js

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p3>Oscar Dempsey    </p3>
+            <p3>    Oscardempseyw@gmail.com</p3>
+            <div className="footer-built-text">Built by OD</div>
+        </footer>
+    );
+};
+
+export default Footer;
